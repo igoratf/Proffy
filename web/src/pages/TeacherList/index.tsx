d72e26157f6999343036323a2602d3d000cb5e42
igor <igor.atf@gmail.com>
2020-08-03 21:33:04 -0300

    add teacher list and responsive styling

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import "./styles.css";
 import PageHeader from "../../components/PageHeader";
+import whatsAppIcon from "../../assets/images/icons/whatsapp.svg";
 
 function TeacherList() {
   return (
@@ -24,6 +25,35 @@ function TeacherList() {
           </div>
         </form>
       </PageHeader>
+
+      <main>
+          <article className="teacher-item">
+              <header>
+                  <img src="https://avatars3.githubusercontent.com/u/23489054?s=460&u=0e0e7e963d782b032fa851c83955cd7f937279a8&v=4" alt="Igor Farias" />
+                  <div>
+                      <strong>Igor Farias</strong>
+                      <span>Programming</span>
+                  </div>
+              </header>
+
+              <p>
+                  Deeply interested in JavaScript ecosystem
+                  <br/> <br/>
+                  Full Stack Developer, mostly focused on front-end development and always fascinated about what is new in tech.
+              </p>
+
+              <footer>
+                  <p>
+                      Preço/hora
+                      <strong>R$ 80,00</strong>
+                  </p>
+                  <button type="button">
+                    <img src={whatsAppIcon} alt="Whatsapp" />
+                    Entrar em contato
+                  </button>
+              </footer>
+          </article>
+      </main>
     </div>
   );
 }
